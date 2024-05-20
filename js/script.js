641b7f85b1c7676344fb511fb98fb5e6927b3af2
raghveerdeol <deolraghveer5@gmail.com>
2024-05-20 15:31:46 +0200

    creata funzione getRandomNumber e aggiunto ciclo while al interno

--- js/script.js
+++ js/script.js
@@ -1,8 +1,24 @@
+// <!-- Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.  -->
+// 1 creo un array bombe vuoto; 
+// 2 per generare i numeri casuali creo una funzione con il parametro celle, al interno della quale uso un ciclo while < 16, al interno del quale genero i numeri casuali compresi tra 1 e il numero delle celle; 
+// <!-- Attenzione: **nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali. -->
+// 3 Per far si che non si ripeta un numero nel array delle bombe uso un if al interno del ciclo while, se il numero generato è già presente nel array genero un'altro numero;
+// 4 una volta che ho il numero lo pusho nel array bombe; 
+// <!-- In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. -->
+// 5 creo un eventListener sul article che rappresenta le celle;
+// 6 creo un un ciclo while < bomba.length al interno del quale uso if per vedere se il numero della cella è uguale a un numero presente nel array bomba, 
+// 7 se è presente aggiungo una classe per colorarla e stampo game over;
+// <!-- Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle. -->
+// 8 else aggiungo una classe per colorarla di azzuro;
+// <!-- La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe). -->
+// 9 se i due numeri prima comparati tra loro sono uguali la partita termina e il giocatore ha perso, oppure il giocatore clicca un numero di celle uguale a celle - 16 allora la partita termina e il giocatore ha vinto;
+// <!-- Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba. -->
+// 10  ogni volta che il giocatore clicca una cella il cui numero non coincide con array bomba al il punteggio diventa punteggio + 1; 
+
 const sectionEl = document.querySelector('section#container');
 const play = document.querySelector('button#play');
 const selectEl = document.getElementById("difficoltà");
-
-
+// creato tasto play che genera la griglia al click;
 play.addEventListener("click", function () {
     console.log(selectEl.value);
     let cellNumber;
@@ -29,7 +45,7 @@ play.addEventListener("click", function () {
 
 })
 
-
+// funzone griglia 
 function getGrid(containerEl, cells, addNewClass) {
     containerEl.innerHTML = "";
     let n = 0;
@@ -51,6 +67,22 @@ function getGrid(containerEl, cells, addNewClass) {
             console.log(articleEl.innerHTML);
         });
         
+        n += 1;
+    }
+}
+// 2 per generare i numeri casuali creo una funzione con il parametro celle, 
+// al interno della quale uso un ciclo while < 16, al interno del quale genero i numeri casuali compresi tra 1 e il numero delle celle
+function getRandomNumber(cells) {
+    // 1 creo un array bombe vuoto; 
+    let bombe = [];
+    let n = 0;
+    while (n < 16) {
+        const randomBomb = Number.Math.floor(Math.random() * cells) + 1;
+        // 3 Per far si che non si ripeta un numero nel array delle bombe uso un if al interno del ciclo while,
+        // se il numero generato è già presente nel array genero un'altro numero;
+        if (randomBomb === bombe[]) {
+            
+        }
         n += 1;
     }
 }
